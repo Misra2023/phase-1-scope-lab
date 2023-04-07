bdd034560627faceead2949fa53638772673f60b
misra2023 <mishazabdi@gmail.com>
2023-04-07 17:20:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'definitely not bob';
+}
+
+const leastFavoriteCustomer = 'Joe';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Steve'; // TypeError: Assignment to constant variable.
+}
